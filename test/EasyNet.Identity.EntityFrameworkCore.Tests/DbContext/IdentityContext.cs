277b904bcba67ac62d86d6d18a0dd7d2fadd12d0
Sid Zhao <sid.zhao@outlook.com>
2020-12-22 17:15:37 +0800

    Fix bug for IdentityContext constructor

--- test/EasyNet.Identity.EntityFrameworkCore.Tests/DbContext/IdentityContext.cs
+++ test/EasyNet.Identity.EntityFrameworkCore.Tests/DbContext/IdentityContext.cs
@@ -1,12 +1,14 @@
-﻿using EasyNet.Identity.EntityFrameworkCore.DbContext;
+﻿using EasyNet.Domain.Uow;
+using EasyNet.Identity.EntityFrameworkCore.DbContext;
 using EasyNet.Identity.EntityFrameworkCore.Tests.Domain.Entities;
+using EasyNet.Runtime.Session;
 using Microsoft.EntityFrameworkCore;
 
 namespace EasyNet.Identity.EntityFrameworkCore.Tests.DbContext
 {
     public class IdentityContext : EasyNetIdentityDbContext<User, Role, int>
     {
-        public IdentityContext(DbContextOptions options) : base(options)
+        public IdentityContext(DbContextOptions options, ICurrentUnitOfWorkProvider currentUnitOfWorkProvider, IEasyNetSession session) : base(options, currentUnitOfWorkProvider, session)
         {
         }
     }
